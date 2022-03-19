e88c46a62cbb85679c5e425da84e22621f14ab6b
Sreenath285 <sreenath.285@gmail.com>
2022-03-19 11:41:34 +0530

    Printing Welcome to Hotel Reservation System

---
+++ HotelReservationSystem/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    /***
+     * main method for manipulation
+     * @param args - default param(not used)
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Reservation System");
+    }
+}
